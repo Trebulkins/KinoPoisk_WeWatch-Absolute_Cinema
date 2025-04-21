ebc865bc7bb97a131352f50fae2265829d284d9e
Trebulkins <trebinivan2005@gmail.com>
2025-04-21 17:52:29 +0700

    main activity re-made

--- app/src/main/java/com/example/absolute_cinema_wewatch/MainActivity.kt
+++ app/src/main/java/com/example/absolute_cinema_wewatch/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.absolute_cinema_wewatch
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var moviesRecyclerView: RecyclerView
+    private lateinit var fab: FloatingActionButton
+    private lateinit var noMoviesLayout: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        moviesRecyclerView = findViewById(R.id.movies_recyclerview)
+        moviesRecyclerView.layoutManager = LinearLayoutManager(this)
+        fab = findViewById(R.id.fab)
+        noMoviesLayout = findViewById(R.id.no_movies_layout)
+        supportActionBar?.title = "@strings/movies_to_watch"
     }
 }
